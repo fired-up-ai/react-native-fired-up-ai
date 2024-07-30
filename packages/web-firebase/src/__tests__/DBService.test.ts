b3cad99946f203603a675b345776601f8c0aea08
sukrut29 <sukrutssathaye@gmail.com>
2024-07-30 13:55:45 +0200

    import package web-firebase from another repo

---
+++ packages/web-firebase/src/__tests__/DBService.test.ts
@@ -0,0 +1,74 @@
+import { 
+    initializeTestEnvironment, 
+    RulesTestEnvironment 
+} from "@firebase/rules-unit-testing";
+
+import DBService from "../services/DBService";
+import AuthService from "../services/AuthService";
+import { UserCredential } from "firebase/auth";
+import { DocumentReference } from "firebase/firestore";
+
+let testEnv: RulesTestEnvironment;
+let userCredential: UserCredential;
+beforeAll(async () => {
+    // Set up the test environment before running the tests.
+    // This will trigger the emulator to start and link with Firebase Project
+    testEnv = await initializeTestEnvironment({
+        projectId: process.env.FIREBASE_PROJECT_ID
+    });
+    const authService: AuthService = new AuthService();
+    userCredential = await authService.signInAnonymously();
+});
+
+afterAll(async () => {
+    // Tear down the test environment after tests are done.
+    // This will trigger the emulator to stop
+    // await testEnv.cleanup()
+});
+
+describe("DBService", () => {
+
+    const dbService: DBService = new DBService();
+    
+    test("createDoc", async () => {
+        const collectionName = "users";
+        const data = {
+            name: "Test User",
+            email: "testuser@gmail.com",
+            age: 25
+        };
+        const docRef = await dbService.createDoc(collectionName, data);
+        expect(docRef).toBeDefined();
+        expect(docRef).toBeInstanceOf(DocumentReference);
+       
+    });
+
+    test("createDocWithId", async () => {
+        const collectionName = "users";
+        const docId = "123456";
+        const data = {
+            name: "Test User",
+            email: "testuser@gmail.com",
+            age: 25
+        };
+        await dbService.createDocWithId(collectionName, docId, data);
+        const doc = await dbService.getDoc(collectionName, docId);
+        expect(doc).toBeDefined();
+    });
+
+        
+
+    test("getDoc", async () => {
+        const collectionName = "users";
+        const docId = "12345";
+        const doc = await dbService.getDoc(collectionName, docId);
+        expect(doc).toBeNull();
+    });
+
+    test("getDocs", async () => {
+        const collectionName = "users";
+        const docs = await dbService.getDocs(collectionName);
+        expect(docs).toBeDefined();
+    });
+
+});
