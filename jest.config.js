33483236e719a2477f9af6b34b897fcf8fb845d9
sukrut29 <sukrutssathaye@gmail.com>
2024-07-30 13:23:30 +0200

    root jest.config for common test configurations

---
+++ jest.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+    preset: 'react-native',
+    setupFilesAfterEnv: ['<rootDir>/../../jest-setup.js'],
+    transformIgnorePatterns: [
+      'node_modules/(?!(jest-)?react-native|@react-native|@react-native-community|@react-navigation|@react-native-polyfills|@expo/vector-icons)'
+    ],
+    transform: {
+      '^.+\\.jsx$': 'babel-jest',
+      '^.+\\.tsx?$': [
+        'ts-jest',
+        {
+          tsconfig: 'tsconfig.spec.json',
+        },
+      ],
+    },
+    moduleNameMapper: {
+      '^@expo/vector-icons$': '<rootDir>/src/components/__mocks__/@expo/vector-icons.js',
+    },
+    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+    collectCoverage: true,
+    coverageDirectory: '<rootDir>/coverage',
+    coverageReporters: ['json', 'lcov', 'text', 'clover'],
+    collectCoverageFrom: [
+      'src/**/*.{js,jsx,ts,tsx}',
+      '!src/**/*.d.ts',
+      '!src/**/index.{js,ts}'
+    ]
+  };
+  
